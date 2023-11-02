a8bfb8bfc4b35f2c8ca0522436c5a00c3506a986
SKGakiria <swgakiria@gmail.com>
2023-11-02 15:15:07 +0000

    Added user_data.csv file

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module containing a hash_password function"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Function that returns a salted, hashed password,
+    which is a byte string"""
+    bc = password.encode()
+    hashed = hashpw(bc, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Function checks whether the provided password matches
+    the hashed password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
