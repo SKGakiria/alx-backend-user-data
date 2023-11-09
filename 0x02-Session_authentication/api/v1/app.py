46b66bb367c14178bf3f5e1ece5fef3a0922bd8c
SKGakiria <swgakiria@gmail.com>
2023-11-09 06:39:08 +0000

    Added Task 1 - Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ import os
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -19,6 +20,8 @@ if AUTH_TYPE == "auth":
     auth = Auth()
 elif AUTH_TYPE == "basic_auth":
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""The Session Authentication class module"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """Class representing Session Authentication"""
