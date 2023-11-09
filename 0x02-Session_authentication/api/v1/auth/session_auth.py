cfce808018b499cc5098d27ca5f0ed968847ae00
SKGakiria <swgakiria@gmail.com>
2023-11-09 06:56:41 +0000

    Added Task 2 - Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 """The Session Authentication class module"""
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Class representing Session Authentication"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        s_id = uuid4()
+        self.user_id_by_session_id[str(s_id)] = user_id
+        return str(s_id)
